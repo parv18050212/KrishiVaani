48cb749c412e5d4ff5e6bc38071f1d0e2edbfe0f
PranavJa1n <pranav.avlok@gmail.com>
2025-08-30 00:43:09 +0530

    1

---
+++ mandi.py
@@ -0,0 +1,2 @@
+url = r"https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&format=json&limit=10 "
+api = "579b464db66ec23bdd00000129bb1eb3e823497b5836d0d562d9081d" 
