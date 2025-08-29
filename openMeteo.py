20b1eb5b198f446a9e471ce20d143fd0cd911ec4
PranavJa1n <pranav.avlok@gmail.com>
2025-08-30 02:30:07 +0530

    imporved openMeteo file

--- openMeteo.py
+++ openMeteo.py
@@ -18,11 +18,10 @@ lat, lon = g.latlng
 # The order of variables in hourly or daily is important to assign them correctly below
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
-	"latitude": 30.7351,
-	"longitude": 79.0669,
-	"daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "rain_sum", "showers_sum", "snowfall_sum"],
-	"hourly": ["temperature_2m", "rain", "snowfall", "cloud_cover", "wind_speed_10m", "soil_temperature_0cm", "soil_temperature_6cm", "soil_moisture_0_to_1cm", "soil_moisture_1_to_3cm", "weather_code", "surface_pressure"],
-	"current": ["temperature_2m", "is_day", "rain", "showers", "snowfall", "precipitation", "relative_humidity_2m", "apparent_temperature", "weather_code", "cloud_cover", "pressure_msl", "surface_pressure", "wind_gusts_10m", "wind_direction_10m", "wind_speed_10m"],
+	"latitude": lat,
+	"longitude": lon,
+	"daily": ["weather_code", "temperature_2m_max", "temperature_2m_min", "rain_sum", "showers_sum", "snowfall_sum", "uv_index_max", "sunshine_duration", "wind_speed_10m_max", "wind_gusts_10m_max"],
+	"hourly": ["temperature_2m", "rain", "snowfall", "cloud_cover", "wind_speed_10m", "soil_temperature_0cm", "soil_temperature_6cm", "soil_moisture_0_to_1cm", "soil_moisture_1_to_3cm", "weather_code", "surface_pressure", "relative_humidity_2m", "precipitation", "showers", "vapour_pressure_deficit", "soil_moisture_3_to_9cm", "soil_moisture_9_to_27cm", "soil_temperature_18cm"],
 	"timezone": "auto",
 	"precipitation_unit": "inch",
 }
@@ -34,41 +33,6 @@ print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
 print(f"Elevation: {response.Elevation()} m asl")
 print(f"Timezone: {response.Timezone()}{response.TimezoneAbbreviation()}")
 print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
-
-# Process current data. The order of variables needs to be the same as requested.
-current = response.Current()
-current_temperature_2m = current.Variables(0).Value()
-current_is_day = current.Variables(1).Value()
-current_rain = current.Variables(2).Value()
-current_showers = current.Variables(3).Value()
-current_snowfall = current.Variables(4).Value()
-current_precipitation = current.Variables(5).Value()
-current_relative_humidity_2m = current.Variables(6).Value()
-current_apparent_temperature = current.Variables(7).Value()
-current_weather_code = current.Variables(8).Value()
-current_cloud_cover = current.Variables(9).Value()
-current_pressure_msl = current.Variables(10).Value()
-current_surface_pressure = current.Variables(11).Value()
-current_wind_gusts_10m = current.Variables(12).Value()
-current_wind_direction_10m = current.Variables(13).Value()
-current_wind_speed_10m = current.Variables(14).Value()
-
-print(f"\nCurrent time: {current.Time()}")
-print(f"Current temperature_2m: {current_temperature_2m}")
-print(f"Current is_day: {current_is_day}")
-print(f"Current rain: {current_rain}")
-print(f"Current showers: {current_showers}")
-print(f"Current snowfall: {current_snowfall}")
-print(f"Current precipitation: {current_precipitation}")
-print(f"Current relative_humidity_2m: {current_relative_humidity_2m}")
-print(f"Current apparent_temperature: {current_apparent_temperature}")
-print(f"Current weather_code: {current_weather_code}")
-print(f"Current cloud_cover: {current_cloud_cover}")
-print(f"Current pressure_msl: {current_pressure_msl}")
-print(f"Current surface_pressure: {current_surface_pressure}")
-print(f"Current wind_gusts_10m: {current_wind_gusts_10m}")
-print(f"Current wind_direction_10m: {current_wind_direction_10m}")
-print(f"Current wind_speed_10m: {current_wind_speed_10m}")
 
 # Process hourly data. The order of variables needs to be the same as requested.
 hourly = response.Hourly()
@@ -83,6 +47,13 @@ hourly_soil_moisture_0_to_1cm = hourly.Variables(7).ValuesAsNumpy()
 hourly_soil_moisture_1_to_3cm = hourly.Variables(8).ValuesAsNumpy()
 hourly_weather_code = hourly.Variables(9).ValuesAsNumpy()
 hourly_surface_pressure = hourly.Variables(10).ValuesAsNumpy()
+hourly_relative_humidity_2m = hourly.Variables(11).ValuesAsNumpy()
+hourly_precipitation = hourly.Variables(12).ValuesAsNumpy()
+hourly_showers = hourly.Variables(13).ValuesAsNumpy()
+hourly_vapour_pressure_deficit = hourly.Variables(14).ValuesAsNumpy()
+hourly_soil_moisture_3_to_9cm = hourly.Variables(15).ValuesAsNumpy()
+hourly_soil_moisture_9_to_27cm = hourly.Variables(16).ValuesAsNumpy()
+hourly_soil_temperature_18cm = hourly.Variables(17).ValuesAsNumpy()
 
 hourly_data = {"date": pd.date_range(
 	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
@@ -102,6 +73,13 @@ hourly_data["soil_moisture_0_to_1cm"] = hourly_soil_moisture_0_to_1cm
 hourly_data["soil_moisture_1_to_3cm"] = hourly_soil_moisture_1_to_3cm
 hourly_data["weather_code"] = hourly_weather_code
 hourly_data["surface_pressure"] = hourly_surface_pressure
+hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+hourly_data["precipitation"] = hourly_precipitation
+hourly_data["showers"] = hourly_showers
+hourly_data["vapour_pressure_deficit"] = hourly_vapour_pressure_deficit
+hourly_data["soil_moisture_3_to_9cm"] = hourly_soil_moisture_3_to_9cm
+hourly_data["soil_moisture_9_to_27cm"] = hourly_soil_moisture_9_to_27cm
+hourly_data["soil_temperature_18cm"] = hourly_soil_temperature_18cm
 
 hourly_dataframe = pd.DataFrame(data = hourly_data)
 print("\nHourly data\n", hourly_dataframe)
@@ -114,6 +92,10 @@ daily_temperature_2m_min = daily.Variables(2).ValuesAsNumpy()
 daily_rain_sum = daily.Variables(3).ValuesAsNumpy()
 daily_showers_sum = daily.Variables(4).ValuesAsNumpy()
 daily_snowfall_sum = daily.Variables(5).ValuesAsNumpy()
+daily_uv_index_max = daily.Variables(6).ValuesAsNumpy()
+daily_sunshine_duration = daily.Variables(7).ValuesAsNumpy()
+daily_wind_speed_10m_max = daily.Variables(8).ValuesAsNumpy()
+daily_wind_gusts_10m_max = daily.Variables(9).ValuesAsNumpy()
 
 daily_data = {"date": pd.date_range(
 	start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
@@ -128,6 +110,10 @@ daily_data["temperature_2m_min"] = daily_temperature_2m_min
 daily_data["rain_sum"] = daily_rain_sum
 daily_data["showers_sum"] = daily_showers_sum
 daily_data["snowfall_sum"] = daily_snowfall_sum
+daily_data["uv_index_max"] = daily_uv_index_max
+daily_data["sunshine_duration"] = daily_sunshine_duration
+daily_data["wind_speed_10m_max"] = daily_wind_speed_10m_max
+daily_data["wind_gusts_10m_max"] = daily_wind_gusts_10m_max
 
 daily_dataframe = pd.DataFrame(data = daily_data)
 print("\nDaily data\n", daily_dataframe)
